85bdaf7abaf194d651e950549cf0f332b2af9d36
Ryuki Kimura <ryuki.kimura@gaxi.co.jp>
2025-11-04 16:37:39 +0900

    自動更新の通知メールを停止

--- ystandard-child/functions.php
+++ ystandard-child/functions.php
@@ -375,3 +375,10 @@ function my_customize_register($wp_customize) {
     )));
 }
 add_action('customize_register', 'my_customize_register');
+
+
+
+// テーマの自動更新の通知メールを停止
+add_filter( 'auto_theme_update_send_email', '__return_false' );
+// プラグインの自動更新の通知メールを停止
+add_filter( 'auto_plugin_update_send_email', '__return_false' );
